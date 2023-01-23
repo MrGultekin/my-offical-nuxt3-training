c14712701d39b0c0f6f938b77b1fdbcef7b51c50
MrGultekin <gultekin@gmail.com>
2023-01-23 13:21:19 +0100

    1-6 Update Lesson Styling

--- pages/course/chapter/[chapterSlug]/lesson/[lessonSlug].vue
+++ pages/course/chapter/[chapterSlug]/lesson/[lessonSlug].vue
@@ -1,22 +1,43 @@
 <template>
-  <h2>Lesson</h2>
-  <p>This is a lesson</p>
-<!--  <p>Chapter slug: {{ $route.params.chapterSlug }}</p>-->
-<!--  <p>Lesson slug: {{ $route.params.lessonSlug }}</p>-->
-  <p>{{ chapter.title }}</p>
-  <p>{{ lesson.title }}</p>
+  <div>
+    <p class="mt-0 uppercase font-bold text-slate-400 mb-1">
+      Lesson {{ chapter.number }} - {{ lesson.number }}
+    </p>
+    <h2 class="my-0">{{ lesson.title }}</h2>
+    <div class="flex space-x-4 mt-2 mb-8">
+      <a
+          v-if="lesson.sourceUrl"
+          class="font-normal text-md text-gray-500"
+          :href="lesson.sourceUrl"
+      >
+        Download Source Code
+      </a>
+      <a
+          v-if="lesson.downloadUrl"
+          class="font-normal text-md text-gray-500"
+          :href="lesson.downloadUrl"
+      >
+        Download Video
+      </a>
+    </div>
+    <p>{{ lesson.text }}</p>
+  </div>
 </template>
 
 <script setup>
-const course = useCourse()
-const route = useRoute() // This is the same as $route
+const course = useCourse();
+const route = useRoute();
 
-const chapter = computed(() =>{
-  return course.chapters.find(chapter => chapter.slug === route.params.chapterSlug)
-})
+const chapter = computed(() => {
+  const {chapterSlug} = route.params;
+  return course.chapters.find(
+      (chapter) => chapter.slug === chapterSlug
+  );
+});
 
 const lesson = computed(() => {
-  const {lessonSlug} = route.params;
-  return chapter.value.lessons.find(lesson => lesson.slug === lessonSlug)
-})
+  return chapter.value.lessons.find(
+      (lesson) => lesson.slug === route.params.lessonSlug
+  );
+});
 </script>
